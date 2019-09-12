da0e13e0a546c64b9d78316a34a6ef5ccd455650
mpuri1982 <mpuri1982@gmail.com>
2019-09-12 10:06:17 +1000

    New commit

---
+++ JavaBasics/src/Basics_Revision/Loops.java
@@ -0,0 +1,18 @@
+package Basics_Revision;
+
+public class Loops {
+
+	public static void main(String[] args) {
+		for(int i=0; i<10;i++)
+		{
+			System.out.println("Value of i is :" +i);
+		}
+		
+		int i=0;
+		while(i<5)
+		{
+			System.out.println("i is less then 5 " + i);
+			i++;
+		}
+	}
+}
